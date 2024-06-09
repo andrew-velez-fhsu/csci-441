03c0948e89eb38b2dd99c84ade0db985af8a8e5b
Andrew Velez <a_velez2@mail.fhsu.edu>
2024-06-09 16:40:49 -0400

    Initial Spring Boot backend code

---
+++ Backend/src/main/java/edu/fhsu/summer/csci441/group1/ZoomBuddy/model/User.java
@@ -0,0 +1,17 @@
+package edu.fhsu.summer.csci441.group1.ZoomBuddy.model;
+
+public class User {
+    private String userName;
+
+    public User(){
+
+    }
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
