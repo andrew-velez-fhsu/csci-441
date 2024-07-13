e9e0fbbf671e7bdead7b306beda89900e378d349
Andrew Velez <andrewjvelez@gmail.com>
2024-07-13 15:08:21 -0400

    Geographic search now working!

--- Backend/src/main/java/edu/fhsu/summer/csci441/group1/ZoomBuddy/controller/SearchController.java
+++ Backend/src/main/java/edu/fhsu/summer/csci441/group1/ZoomBuddy/controller/SearchController.java
@@ -6,21 +6,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.fhsu.summer.csci441.group1.ZoomBuddy.data.PetsRepository;
+import edu.fhsu.summer.csci441.group1.ZoomBuddy.data.UsersRepository;
 import edu.fhsu.summer.csci441.group1.ZoomBuddy.model.Pet;
 import edu.fhsu.summer.csci441.group1.ZoomBuddy.service.ApiLayerService;
 import edu.fhsu.summer.csci441.group1.ZoomBuddy.service.IpChecker;
 import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
 
 @RestController
 @CrossOrigin
 public class SearchController {
     private final PetsRepository petsRepository;
     private final ApiLayerService apiLayerService;
+    private final UsersRepository usersRepository;
     private static double MetersPerMile = 1609.34;
 
-    public SearchController(PetsRepository petsRepository, ApiLayerService apiLayerService) {
+    public SearchController(PetsRepository petsRepository, ApiLayerService apiLayerService,
+            UsersRepository usersRepository) {
         this.petsRepository = petsRepository;
         this.apiLayerService = apiLayerService;
+        this.usersRepository = usersRepository;
     }
 
     @GetMapping("/search")
@@ -51,8 +57,19 @@ public class SearchController {
 
         var radiusInMeters = radiusInMiles * MetersPerMile;
 
+        // get all users in search radius
+        var users = usersRepository.GetUsersByDistanceFromLocation(radiusInMeters, latitude, longitude);
+
+        // get all users' pets
+        var foundPets = new ArrayList<Pet>();
+        for (var user : users) {
+            var userPets = petsRepository.findAllPetsByUser(user.getUid());
+            for (var userPet : userPets)
+                foundPets.add(userPet);
+        }
+
         // stub, need a real search
-        var foundPets = this.petsRepository.findAll();
+        // var foundPets = this.petsRepository.findAll();
 
         // TODO: redact all information except name, breed, first photo, and the first
         // 200 characters of the description
--- Backend/src/main/java/edu/fhsu/summer/csci441/group1/ZoomBuddy/data/UsersRepository.java
+++ Backend/src/main/java/edu/fhsu/summer/csci441/group1/ZoomBuddy/data/UsersRepository.java
@@ -1,7 +1,15 @@
 package edu.fhsu.summer.csci441.group1.ZoomBuddy.data;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import edu.fhsu.summer.csci441.group1.ZoomBuddy.model.User;
 
 public interface UsersRepository extends CrudRepository<User, String> {
 
+    @Query(value = "SELECT * FROM users WHERE ST_DWithin(location, ST_MakePoint(:longitude, :latitude), :radius)", nativeQuery = true)
+    public List<User> GetUsersByDistanceFromLocation(@Param("radius") Double radius, @Param("latitude") Double latitude,
+            @Param("longitude") Double longitude);
 }
